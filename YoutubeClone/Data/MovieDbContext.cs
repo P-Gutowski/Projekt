ce843f5d19f4d897b9e8c5fe2bb415af4d790963
MarcinMichnik <marcinmichnik1@gmail.com>
2022-11-23 21:06:18 +0100

    Works with database

--- YoutubeClone/Data/MovieDbContext.cs
+++ YoutubeClone/Data/MovieDbContext.cs
@@ -1,24 +1,16 @@
-﻿using System.Data.Entity;
-using System.Data.Entity.ModelConfiguration.Conventions;
-using System.Runtime.Intrinsics.Arm;
+﻿using Microsoft.EntityFrameworkCore;
 using YoutubeClone.Models;
 
 namespace YoutubeClone.Data
 {
     public class MovieDbContext : DbContext
     {
-        public MovieDbContext() : base("MovieDbContext")
-        {
-        }
-
         public DbSet<Movie> Movies { get; set; }
         public DbSet<Comment> Comments { get; set; }
         public DbSet<Rating> Ratings { get; set; }
         public DbSet<Tag> Tags { get; set; }
 
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql("User ID=postgres;Password=postgres;Server=localhost;Port=5432;Database=youtube_clone;");
     }
 }
