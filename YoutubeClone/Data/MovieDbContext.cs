17607f94e3ef755082f1942ebec3ff11fa5de1a7
MarcinMichnik <marcinmichnik1@gmail.com>
2022-11-24 21:48:20 +0100

    Controllers

--- YoutubeClone/Data/MovieDbContext.cs
+++ YoutubeClone/Data/MovieDbContext.cs
@@ -5,6 +5,7 @@ namespace YoutubeClone.Data
 {
     public class MovieDbContext : DbContext
     {
+        public MovieDbContext(DbContextOptions<MovieDbContext> options) : base(options) { }
         public DbSet<Movie> Movies { get; set; }
         public DbSet<Comment> Comments { get; set; }
         public DbSet<Rating> Ratings { get; set; }
